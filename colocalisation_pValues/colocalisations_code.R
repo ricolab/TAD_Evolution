1ea373afcad79214a368bb6004c01acfbe8e0a75
Marco Trevisan-Herraz <nmt106@newcastle.ac.uk>
2021-02-09 18:49:06 +0000

    Bugs fixed and three human cell types added

--- colocalisation_pValues/colocalisations_code.R
+++ colocalisation_pValues/colocalisations_code.R
@@ -1,12 +1,12 @@
 source("colocalisations_functions.R")
 library("biomaRt")
 
-runTADEvolution = function(species, TADFile, ageFile, ageList, replacementTable, analysisName, totRandomisations = 15) {
+runTADEvolution = function(species, TADFile, ageFile, ageList, replacementTable, analysisName, totRandomisations = 15, removePreChr = TRUE) {
 	ensemblDataset = "hsapiens_gene_ensembl"
 	if(species == "Mus_musculus") ensemblDataset = "mmusculus_gene_ensembl"
 
-	myTADs = read.table(TADFile)
-	myTADs$chr = substr(myTADs$chr, 4, length(myTADs$chr) - 3)
+	myTADs = read.table(TADFile, header = TRUE)
+	if(removePreChr) myTADs$chr = substr(myTADs$chr, 4, length(myTADs$chr) - 3)
 
 	ensemblData = useMart(host='feb2014.archive.ensembl.org',
 							biomart='ENSEMBL_MART_ENSEMBL',
@@ -20,9 +20,9 @@ runTADEvolution = function(species, TADFile, ageFile, ageList, replacementTable,
 		geneAgeList = geneAgeList[,1:6]
 		}
 	if (species == "Mus_musculus") {
-		geneAgeList = as.data.frame(cbind(ensembl_gene_id = as.character(geneAgeList$GeneID), HUGO_symbol = NA, seqnames = NA, start = NA, end = NA, GeneAge = as.character(geneAgeListPre$GeneAge)))
+		geneAgeList = as.data.frame(cbind(ensembl_gene_id = as.character(geneAgeList$GeneID), HUGO_symbol = NA, seqnames = NA, start = NA, end = NA, GeneAge = as.character(geneAgeList$GeneAge)))
 		}
-
+	print(head(myTADs))
 	newTADList = getTADGenesTable(allBioMart, myTADs)
 	geneTADAgeTable = mergeGeneTADAge(newTADList, geneAgeList)
 	geneTADAgeTableAgeOnly = subset(geneTADAgeTable, gene_age != "No Age Provided")
@@ -74,4 +74,48 @@ runTADEvolution(
 			ageList = ageListHuman,
 			replacementTable = replacementTableHuman,
 			analysisName = "hESC"
+			)
+			
+# Bcell merging Primates
+runTADEvolution(
+			species = "Homo_sapiens",
+			TADFile = "GSE63525_GM12878_primary+replicate_Arrowhead_domainlist_sorted_noNested.txt",
+			ageFile = "AgesWithChrX.tsv", # converted from Caelinn's file in /data
+			ageList = ageListHuman,
+			replacementTable = replacementTableHuman,
+			analysisName = "hBcell",
+			removePreChr = FALSE
+			)
+			
+# nCD4 merging Primates
+runTADEvolution(
+			species = "Homo_sapiens",
+			TADFile = "TADs_nCD4_mean_merged.bed",
+			ageFile = "AgesWithChrX.tsv", # converted from Caelinn's file in /data
+			ageList = ageListHuman,
+			replacementTable = replacementTableHuman,
+			analysisName = "nCD4",
+			removePreChr = FALSE
+			)
+			
+# monocytes merging Primates
+runTADEvolution(
+			species = "Homo_sapiens",
+			TADFile = "TADs_Mon_mean_merged.bed",
+			ageFile = "AgesWithChrX.tsv", # converted from Caelinn's file in /data
+			ageList = ageListHuman,
+			replacementTable = replacementTableHuman,
+			analysisName = "mono",
+			removePreChr = FALSE
+			)
+			
+# neutrophils merging Primates
+runTADEvolution(
+			species = "Homo_sapiens",
+			TADFile = "TADs_Neu_mean_merged.bed",
+			ageFile = "AgesWithChrX.tsv", # converted from Caelinn's file in /data
+			ageList = ageListHuman,
+			replacementTable = replacementTableHuman,
+			analysisName = "neut",
+			removePreChr = FALSE
 			)
